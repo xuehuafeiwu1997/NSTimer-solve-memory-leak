74aa53d1b8234a82ab63370a1bd01f8f81e0f66a
xumingyang <xumingyang@greatyun.com>
2020-09-17 10:49:47 +0800

    测试了使用block的情况下，repeat为YES，使用weakSelf可以解决循环泄漏的问题

--- NSTimer解决循环泄漏/SecondViewController.m
+++ NSTimer解决循环泄漏/SecondViewController.m
@@ -23,8 +23,19 @@
     self.title = @"第二个控制器";
     self.view.backgroundColor = [UIColor whiteColor];
     
-    self.timer = [NSTimer timerWithTimeInterval:1.0 target:self selector:@selector(timerRun) userInfo:nil repeats:YES];
+    /**
+     只有repeats为YES时重复触发定时器时才会出现内存泄漏的问题
+     */
+    self.timer = [NSTimer timerWithTimeInterval:1.0 target:self selector:@selector(timerRun) userInfo:nil repeats:NO];
     [[NSRunLoop currentRunLoop] addTimer:self.timer forMode:NSDefaultRunLoopMode];
+    /**
+     下面的这种带block的写法，当repeat为YES时，如果不使用weakSelf时，也是存在内存泄漏，但是当使用的是weakSelf时，内存泄漏就不存在了。
+     */
+//    __weak typeof(self) weakSelf = self;
+//    self.timer = [NSTimer timerWithTimeInterval:1.0 repeats:YES block:^(NSTimer * _Nonnull timer) {
+//        [weakSelf timerRun];
+//    }];
+    
 }
 
 - (void)timerRun {
